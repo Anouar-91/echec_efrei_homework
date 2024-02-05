ae3c517834d5ac405afcd579392d01acbef33f60
Anouar-91 <a.berrouane@hotmail.com>
2024-02-05 16:04:06 +0100

    add new attribute and relation with player

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GameRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -25,6 +27,25 @@ class Game
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     private ?\DateTimeInterface $updatedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'blackPlayer')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Player $whitePlayer = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Player $blackPlayer = null;
+
+    #[ORM\ManyToOne]
+    private ?Player $winner = null;
+
+    #[ORM\OneToMany(mappedBy: 'game', targetEntity: Move::class)]
+    private Collection $moves;
+
+    public function __construct()
+    {
+        $this->moves = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -75,6 +96,72 @@ class Game
     public function setUpdatedAt(?\DateTimeInterface $updatedAt): static
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getWhitePlayer(): ?Player
+    {
+        return $this->whitePlayer;
+    }
+
+    public function setWhitePlayer(?Player $whitePlayer): static
+    {
+        $this->whitePlayer = $whitePlayer;
+
+        return $this;
+    }
+
+    public function getBlackPlayer(): ?Player
+    {
+        return $this->blackPlayer;
+    }
+
+    public function setBlackPlayer(?Player $blackPlayer): static
+    {
+        $this->blackPlayer = $blackPlayer;
+
+        return $this;
+    }
+
+    public function getWinner(): ?Player
+    {
+        return $this->winner;
+    }
+
+    public function setWinner(?Player $winner): static
+    {
+        $this->winner = $winner;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Move>
+     */
+    public function getMoves(): Collection
+    {
+        return $this->moves;
+    }
+
+    public function addMove(Move $move): static
+    {
+        if (!$this->moves->contains($move)) {
+            $this->moves->add($move);
+            $move->setGame($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMove(Move $move): static
+    {
+        if ($this->moves->removeElement($move)) {
+            // set the owning side to null (unless already changed)
+            if ($move->getGame() === $this) {
+                $move->setGame(null);
+            }
+        }
 
         return $this;
     }
